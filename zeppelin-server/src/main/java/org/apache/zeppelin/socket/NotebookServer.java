4ca0e046cb7cdea78d3fc5fed5e3eb0c807f9a50
vmaster <vmaster.cc@gmail.com>
2022-07-08 16:14:53 +0800

    [modify]服务重启后尚未登录的情况下无效Ticket响应SessionLogout消息

--- zeppelin-server/src/main/java/org/apache/zeppelin/socket/NotebookServer.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/socket/NotebookServer.java
@@ -263,6 +263,10 @@ public class NotebookServer extends WebSocketServlet
       TicketContainer.Entry ticketEntry = TicketContainer.instance.getTicketEntry(receivedMessage.principal);
       if (ticketEntry == null || StringUtils.isEmpty(ticketEntry.getTicket())) {
         LOG.debug("{} message: invalid ticket {}", receivedMessage.op, receivedMessage.ticket);
+        if (!receivedMessage.op.equals(OP.PING)) {
+          conn.send(serializeMessage(new Message(OP.SESSION_LOGOUT).put("info", "Your principal is invalid possibly due to server restart. Please login again.")));
+        }
+
         return;
       } else if (!ticketEntry.getTicket().equals(receivedMessage.ticket)) {
         /* not to pollute logs, log instead of exception */
